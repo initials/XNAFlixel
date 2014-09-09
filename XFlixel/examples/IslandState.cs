e67b6aa9d1f43c40c670e117e4b325f17aeae74e
Shane Brouwer <shane@findesign.com.au>
2014-09-09 12:42:55 +1000

    x

--- XFlixel/examples/IslandState.cs
+++ XFlixel/examples/IslandState.cs
@@ -75,7 +75,7 @@ namespace org.flixel
                     //string toPrint = tiles[y, i];
 
                     FlxSprite x = new FlxSprite(y* 8, i * 8);
-                    x.createGraphic(6, 6, colors[Convert.ToInt32(caveLevel[y, i])]);
+                    x.createGraphic(8, 8, colors[Convert.ToInt32(caveLevel[y, i])]);
                     tileGrp.add(x);
                     //Console.Write(toPrint);
                 }
@@ -87,53 +87,6 @@ namespace org.flixel
 
 
             add(tileGrp);
-
-        }
-
-
-        public void makeCave(float Scroll, Color Col)
-        {
-            // make a new cave of tiles 50x40;
-            FlxCaveGenerator cav = new FlxCaveGenerator(50, 40, 0.48f, 5);
-
-            //Create a matrix based on these parameters.
-            int[,] matr = cav.generateCaveLevel(3, 0, 2, 0, 1, 1, 1, 1);
-
-            //convert the array to a comma separated string
-            string newMap = cav.convertMultiArrayToString(matr);
-
-            //Create a tilemap and assign the cave map.
-            tiles = new FlxTilemap();
-            tiles.auto = FlxTilemap.AUTO;
-            tiles.loadMap(newMap, FlxG.Content.Load<Texture2D>("flixel/autotiles_16x16"), 16, 16);
-            tiles.setScrollFactors(Scroll, Scroll);
-            tiles.color = Col;
-
-            add(tiles);
-
-
-        }
-
-
-        public void makeCave2(float Scroll, Color Col)
-        {
-            FlxCaveGeneratorExt caveExt = new FlxCaveGeneratorExt(50, 40, 0.49f, 5);
-            string[,] caveLevel = caveExt.generateCaveLevel();
-
-            //Optional step to print cave to the console.
-            //caveExt.printCave(caveLevel);
-
-            string newMap = caveExt.convertMultiArrayStringToString(caveLevel);
-
-            //Create a tilemap and assign the cave map.
-            tiles = new FlxTilemap();
-            tiles.auto = FlxTilemap.STRING;
-            tiles.loadMap(newMap, FlxG.Content.Load<Texture2D>("flixel/autotiles_16x16"), 16, 16);
-            tiles.setScrollFactors(Scroll, Scroll);
-            tiles.color = Col;
-            tiles.boundingBoxOverride = true;
-            add(tiles);
-
 
         }
 
