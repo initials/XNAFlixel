f7e8b8040f382599753bb126ec4a36b1bd408e26
Shane Brouwer <shane@findesign.com.au>
2017-01-11 17:39:52 +1100

    add firstLetterToUpper

--- XFlixel/flixel/FlxU.cs
+++ XFlixel/flixel/FlxU.cs
@@ -1529,5 +1529,16 @@ namespace org.flixel
             return p;
         }
 
+        public static string firstLetterToUpper(string str)
+        {
+            if (str == null)
+                return null;
+
+            if (str.Length > 1)
+                return char.ToUpper(str[0]) + str.Substring(1);
+
+            return str.ToUpper();
+        }
+
     }
 }
